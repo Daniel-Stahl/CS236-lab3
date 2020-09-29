e212f627a8e76c364705fbf5a0f1c03b9fc1a03f
Daniel Stahl <danny.stahl@gmail.com>
2020-09-29 14:31:43 -0700

    initializing lab

---
+++ header.hpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class BTreeNode {
+public:
+    BTreeNode(double x, BTreeNode *leftp = NULL, BTreeNode *rightp = NULL) {
+        value = x;
+        left = leftp;
+        right = rightp;
+    }
+              
+private:
+    double value;
+    BTreeNode *left, *right;
+    friend class BST;
+};
+
+class BST {
+public:
+    int height() { return height(tree); }
+    void insert(double x);
+    void inorder(vector<double> & v) { inorder(v, tree); }
+    int leafCounter() { return leafCounter(tree); }
+    BST() { tree = NULL; }
+    
+private:
+    void inorder(vector<double>&tlist, BTreeNode *t);
+    int leafCounter(BTreeNode *t);
+    static int height(BTreeNode *t);
+    BTreeNode *tree;
+};
+              
+              
---
+++ main.cpp
@@ -0,0 +1,11 @@
+/*
+Stahl, Daniel
+CS236, Section #25704
+Fall 2020
+*/
+
+#include "header.hpp"
+
+int main() {
+    
+}
