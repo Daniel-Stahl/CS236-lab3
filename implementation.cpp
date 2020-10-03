37e613a1135d3114451cec6864a87e52ef64bc40
Daniel Stahl <danny.stahl@gmail.com>
2020-10-03 14:10:04 -0700

    insert function is done

--- implementation.cpp
+++ implementation.cpp
@@ -1,1 +1,45 @@
 #include "header.hpp"
+int BST::height(BTreeNode *t) {
+    if (t == nullptr) {
+        return 0;
+    } else {
+        int left = height(t->left);
+        int right = height(t->right);
+        
+        return 1 + max(left, right);
+    }
+}
+
+void BST::insert(double x) {
+    BTreeNode* root = tree;
+    bool nodeEmpty = true;
+    
+    while (nodeEmpty) {
+        if (tree == NULL) {
+            tree = new BTreeNode(x, NULL, NULL);
+            nodeEmpty = false;
+        } else if(x < root->value) {
+            if (root->left == NULL) {
+                root->left = new BTreeNode(x, NULL, NULL);
+                nodeEmpty = false;
+            } else {
+                root = root->left;
+            }
+        } else if(x >= root->value) {
+            if (root->right == NULL) {
+                root->right = new BTreeNode(x, NULL, NULL);
+                nodeEmpty = false;
+            } else {
+                root = root->right;
+            }
+        }
+    }
+}
+
+void BST::inorder(vector<double> & v, BTreeNode *t) {
+    
+    
+    // Cout in order
+}
+
+
--- main.cpp
+++ main.cpp
@@ -7,5 +7,12 @@ Fall 2020
 #include "header.hpp"
 
 int main() {
+    BST Tree;
     
+    Tree.insert(5);
+    Tree.insert(4);
+    Tree.insert(3);
+    Tree.insert(1);
+    Tree.insert(6);
+    Tree.insert(2);
 }
